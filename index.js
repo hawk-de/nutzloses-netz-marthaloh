d9cc03ed59f8464713aebb9f0056c4d944db8eec
marthaloh <martha.lohse@stud.hawk.de>
2022-07-14 19:21:00 +0200

    Add cursor + counter

--- index.js
+++ index.js
@@ -12,44 +12,42 @@
 
 const text1 = "Hallo! Ich bin ein nutzloser Texteditor. Hier kannst du deine Texte eintippen. Probier es aus! Finger auf die Tastatur und los geht es! Keine Scheu, schreib einfach drauf los.  Unterbrich mich ruhig. Doch sei gewarnt: Verärgere mich nicht zu sehr, sonst verweigere ich mich. Und lass mich nicht zu lange warten, ich bin sehr  ungeduldig und werde bei Langeweile schnell schludrig.";
 const text = text1.slice();
-let i = 0;
-let nextletter = false;
+let i = 1;
+let letter = 0;
 let editortext = [];
 let showntext;
-let timecount;
 
 let firstchar;
 let newtext;
 const texteditor = document.getElementById('texteditor');
+const cursor = document.getElementById('cursor');
+const counter = document.getElementById('counter');
+const body = document.getElementById('body');
+const div = document.getElementById('div');
+let deletedletters = 0;
 let indent = 0;
 let background = 255;
 let bgr;
-let ind;
+let ind = indent + "vw";
 
 document.addEventListener('keydown', typeToDelete);
 //document.addEventListener('DOMContentLoaded', runningText);
-
+counter.innerHTML = deletedletters;
 
 function draw() {
-  timecount = frameCount/20;
-  
-  if(timecount === i+1) {
-    nextletter = true;
-  }
 
-  if(nextletter === true) {
-    editortext.push(text[i]);
+  if(frameCount >= i) {
+    editortext.push(text[letter]);
     showntext = editortext.join('');
     texteditor.innerHTML = showntext;
-    i++;
-    nextletter = false;
-    //console.log(editortext);
+    letter++;
+    i += random(14,15);
   }
 
   firstchar = editortext.slice(0,1);
   newtext = editortext;
 
-  if(editortext.length === 0 && timecount > 1){
+  if(editortext.length === 0 && frameCount > 1){
     noLoop();
     document.removeEventListener('keydown', typeToDelete);
     document.addEventListener('keydown', typeToWrite);
@@ -60,7 +58,7 @@ function draw() {
 function typeToDelete(e) {
   indent = 0; 
   ind = indent + "vw";
-  document.getElementById('texteditor').style.setProperty("text-indent", i);
+  div.style.setProperty("text-indent", ind);
 
   console.log(e.key);
 
@@ -70,58 +68,44 @@ function typeToDelete(e) {
     texteditor.innerHTML = showntext;
     firstchar = editortext.slice(0,1);
     newtext = editortext.slice(1, editortext.length);
+    deletedletters++;
+    counter.innerHTML = deletedletters;
   } else if(background > 50 && e.key !== "Shift"){
     background = background - 25;
     bgr = "rgb(" + background + "," + background + "," + background + ")";
-    document.getElementById('body').style.setProperty("background-color", bgr);
+    body.style.setProperty("background-color", bgr);
+  }
+
+  if(background <= 50){
+    counter.style.setProperty("color", "white");
   }
 
   if(firstchar[0] === " "){
     indent = 1.2; 
-    let i = indent + "vw";
-    document.getElementById('texteditor').style.setProperty("text-indent", i);
+    ind = indent + "vw";
+    div.style.setProperty("text-indent", ind);
   }
 }
 
 function typeToWrite(e) {
+  if(background <= 50){
+    texteditor.style.setProperty("color", "white");
+    cursor.style.setProperty("color", "white");
+  }
+  
   if(e.key === "Shift"){
-    document.getElementById('texteditor').style.setProperty("color", "blue");
+    texteditor.style.setProperty("color", "blue");
   } else if(e.key === "Alt"){
-    document.getElementById('texteditor').style.setProperty("color", "red");
+    texteditor.style.setProperty("color", "red");
   } else if(e.key === "Control"){
-    document.getElementById('texteditor').style.setProperty("color", "green");
+    texteditor.style.setProperty("color", "green");
+  } else if(e.key === "Backspace"){
+    editortext.shift();
+    showntext = editortext.join('');
+    texteditor.innerHTML = showntext;
   } else{
     editortext.push(e.key);
     showntext = editortext.join('');
     texteditor.innerHTML = showntext;
   }
 }
-
-
-
-//function fetchSource(){
-  // source_input.value = "kernel";
-  //fetch("./kernel.txt").then(a=>a.text()).then(a=>{index=0,typer.innerHTML="",setStore("source",a),source=a})}
-
-//function toggleCursor(){
-  //cursor.style.color="transparent"===cursor.style.color?"inherit":"transparent"
-//}
-
-//function bindEvents(){
-  //setInterval(toggleCursor,500);
-//}
-
-//function typeToDelete(){
-  //editortext = document.getElementById("texteditor");
-  //textslice = editortext.slice(0,1);
-  //console.log(textslice);
-//}
-
-//function setup() {
-  //bindEvents;
-  //document.getElementById("texteditor").addEventListener("keydown", typeToDelete);
-//}
-
-//function draw() {
-  //typeToDelete;
-//}
